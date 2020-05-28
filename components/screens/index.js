7e4f5c723ed48092c5c4177299b8195507628498
Deniz <dguler@umass.edu>
2020-05-28 15:31:32 -0400

    updated formatting

--- components/screens/index.js
+++ components/screens/index.js
@@ -1,3 +1,3 @@
-export {default as HomeScreen} from "./HomeScreen";
-export {default as LogScreen} from "./LogScreen";
-export {default as SettingsStack} from "./SettingsScreen";
+export { default as HomeScreen } from "./HomeScreen";
+export { default as LogScreen } from "./LogScreen";
+export { default as SettingsStack } from "./SettingsScreen";
