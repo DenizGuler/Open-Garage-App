9a2a22e0b044632b04a68c721895aa71f1312aca
Deniz <dguler@umass.edu>
2020-06-09 16:51:10 -0400

    added more comments and a new way to get device URLs

--- components/screens/utils.js
+++ components/screens/utils.js
@@ -5,8 +5,8 @@ import { useNavigation } from '@react-navigation/native';
 
 /*
   device obj{
-    id: number,
-    OGIP: string,
+    conMethod: 'IP' | 'OTF',
+    conInput: string,
     devKey: string,
   }
 */
@@ -24,11 +24,32 @@ export const setDevices = async (devArr) => {
 }
 
 // Set the current device to the one at the provided index
-// setCurrDev(index: number): void
-export const setCurrDev = async (index) => {
+// setCurrIndex(index: number): void
+export const setCurrIndex = async (index) => {
   try {
-    await AsyncStorage.setItem('currDev', JSON.stringify(index))
+    await AsyncStorage.setItem('currIndex', JSON.stringify(index))
   } catch (err) {
+    console.log(err)
+  }
+}
+
+// set the current device with the provided param and value
+// setCurrDeviceParam(param: string, val: any): void
+export const setCurrDeviceParam = async (param, val) => {
+  let newDevs;
+  try {
+    const [currIdx, devices] = await getDevices();
+    if (devices === null) {
+      newDevs = [{}];
+    } else {
+      newDevs = devices.slice()
+    }
+    if (newDevs[currIdx] === undefined) {
+      newDevs[currIdx] = {};
+    }
+    newDevs[currIdx][param] = val
+    await setDevices(newDevs);
+  } catch {
     console.log(err)
   }
 }
@@ -39,14 +60,14 @@ export const setCurrDev = async (index) => {
 // getDevices(): number, device[]
 export const getDevices = async () => {
   try {
-    const currDev = await AsyncStorage.getItem('currDev');
+    const currIndex = await AsyncStorage.getItem('currIndex');
     const devices = await AsyncStorage.getItem('devices');
     if (devices === null) {
-      await setCurrDev(0);
+      await setCurrIndex(0);
       return [0, null];
     }
     // console.log(currDev + ", " + devices);
-    return [JSON.parse(currDev), JSON.parse(devices)];
+    return [JSON.parse(currIndex), JSON.parse(devices)];
   } catch (err) {
     console.log(err);
   }
@@ -56,21 +77,47 @@ export const getDevices = async () => {
 // getDevKey(): string
 export const getDevKey = async () => {
   try {
-    const [currDev, devices] = await getDevices();
-    return devices[currDev].devKey;
+    const [currIdx, devices] = await getDevices();
+    return devices[currIdx].devKey;
   } catch (err) {
     console.log(err);
   }
 }
 
-// Get the IP of the current device
-// getOGIP(): string
-export const getOGIP = async () => {
+// Get the inputted value for connectivity
+// getConInput(): string
+export const getConInput = async () => {
   try {
-    const [currDev, devices] = await getDevices();
-    return devices[currDev].OGIP;
+    const [currIdx, devices] = await getDevices();
+    return devices[currIdx].conInput;
   } catch (err) {
     console.log(err);
+  }
+}
+
+// Get the url of the device at the given index (or the current device if no index is given)
+// getURL(index?: number): string
+export const getURL = async (index) => {
+  try {
+    let url;
+    const [currIdx, devices] = await getDevices();
+    index = (index === undefined) ? currIdx : index
+    let device = devices[index]
+
+    switch (device.conMethod) {
+      case 'IP':
+        url = 'http://' + device.conInput
+        break;
+      case 'OTF':
+        url = 'https://cloud.test.openthings.io/forward/v1/' + device.conInput
+        break;
+      default:
+        url = ''
+        break;
+    }
+    return url;
+  } catch (err) {
+    console.log(err)
   }
 }
 
@@ -80,9 +127,9 @@ export const getOGIP = async () => {
 // removeDev(index: number): device
 export const removeDev = async (index) => {
   try {
-    const [currDev, devices] = await getDevices();
-    if (currDev === index) {
-      setCurrDev(0);
+    const [currIdx, devices] = await getDevices();
+    if (currIdx === index) {
+      setCurrIndex(0);
     }
     let deleted = devices.splice(index, 1)
     await setDevices(devices);
@@ -94,6 +141,25 @@ export const removeDev = async (index) => {
 
 // COMPONENTS
 
+/* 
+  Header component for screens
+
+  props: {
+    left?={type}
+    text?={string}
+    right?={type}
+    onCheck?={function}
+    onAdd?={function}
+  }
+
+  type: {
+    'hamburger': menu hamburger; opens the navigation drawer,
+    'back': back button; invokes navigate.goBack(),
+    'home' : home button; navigates to the 'Home' screen,
+    'check' : check button; invokes onCheck(),
+    'add' : plus/add button; invokes onAdd(),
+  }
+*/
 export const ScreenHeader = (props) => {
   const navigation = useNavigation();
   const HeaderComponent = (type) => {
