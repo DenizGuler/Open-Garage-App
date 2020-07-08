2489167af0639e2f51bdd07d6f4fc661ec0ad0c1
Deniz <dguler@umass.edu>
2020-07-08 13:57:55 -0400

    updated index file

--- components/index.tsx
+++ components/index.tsx
@@ -3,9 +3,11 @@
 import GarageDrawerComponent from './GarageDrawerComponent';
 import FullLengthButton from './FullLengthButton';
 import ScreenHeader from './ScreenHeader';
+import BottomDraggable from './BottomDraggable';
 
 export {
   GarageDrawerComponent,
   FullLengthButton,
   ScreenHeader,
+  BottomDraggable,
 }
