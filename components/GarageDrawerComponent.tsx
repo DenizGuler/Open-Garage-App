3d49c478bb396f0e3e631aa86d03cc4e1c84bb69
Deniz <dguler@umass.edu>
2020-07-08 13:57:35 -0400

    switched to using custom components for drawer

--- components/GarageDrawerComponent.tsx
+++ components/GarageDrawerComponent.tsx
@@ -1,10 +1,48 @@
-import { useIsDrawerOpen, DrawerContentOptions, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import { useIsDrawerOpen, DrawerContentOptions } from "@react-navigation/drawer";
 import React, { FC, useEffect, useState } from "react";
 import { getImage } from "../screens/utils";
 import { ImageInfo } from "expo-image-picker/build/ImagePicker.types";
 import { DrawerDescriptorMap, DrawerNavigationHelpers } from "@react-navigation/drawer/lib/typescript/src/types";
 import { DrawerNavigationState } from "@react-navigation/native";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import FullLengthButton from "./FullLengthButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type GarageDrawerItemProps = {
+  active: boolean,
+  label: string,
+  onPress: () => void,
+  // icon: string,
+}
+
+const GarageDrawerItem: FC<GarageDrawerItemProps> = (props) => {
+  let icon = ''
+  switch (props.label) {
+    case 'Home':
+      icon = 'home';
+      break;
+    case 'Settings':
+      icon = 'settings';
+      break;
+    case 'Logs':
+      icon = 'script';
+      break;
+    case 'Sites':
+      icon = 'swap-horizontal-bold';
+      break;
+    default:
+      break;
+  }
+  return (
+    <FullLengthButton
+      text={props.label}
+      icon={{ name: icon }}
+      onPress={props.onPress}
+      style={{height: 50, fontSize: 16}}
+      backgroundColor={props.active ? '#e5e5e5' : undefined}
+    />)
+}
 
 const GarageDrawerComponent: FC<DrawerContentOptions & {
   state: DrawerNavigationState;
@@ -15,44 +53,37 @@ const GarageDrawerComponent: FC<DrawerContentOptions & {
   const [image, setImage] = useState<ImageInfo>();
 
   useEffect(() => {
+    // addEventListener('image', (img) => {})
+    // removeEventListener('image', () => {})
     getImage().then((img) => {
       if (img !== undefined)
         setImage(img)
     })
   }, [isDrawerOpen])
 
+  // console.log(props.state)
+
   return (
-    <DrawerContentScrollView {...props} style={{ zIndex: -1 }}>
+    <SafeAreaView {...props}>
       {image && <Image source={{ uri: image.uri }} style={{
         height: 200,
-        width: image.width / image.height * 200,
+        width: Platform.OS === 'web' ? 310 : image.width / image.height * 200,
         alignSelf: 'center',
         borderRadius: 5,
-        marginBottom: 5,
+        marginVertical: 5,
       }} />}
-      <DrawerItemList
-        {...props}
-        // navigation={props.navigation}
-        itemStyle={{
-          width: '100%',
-          marginHorizontal: 0,
-          marginVertical: 0,
-          borderRadius: 0,
-          borderColor: '#adacac',
-          borderBottomWidth: 1,
-          paddingLeft: 0,
-        }}
-        labelStyle={{
-          // position: 'absolute',
-          left: -15,
-          bottom: -10,
-          top: 0,
-          // top: '50%'
-        }}
-        activeBackgroundColor='#adacac'
-        activeTintColor='#fff'
+      <FlatList
+        data={props.state.routes}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <GarageDrawerItem 
+            active={(props.state.routeNames[props.state.index]) === item.name}
+            label={item.name} 
+            onPress={() => { props.navigation.navigate(item.name) }} 
+          />
+        )}
       />
-    </DrawerContentScrollView>
+    </SafeAreaView>
   )
 }
 
